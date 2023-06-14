d32d5fe80515cb763533bbe8bd674761e972fcb8
Reuben Miller <reuben.d.miller@gmail.com>
2023-06-14 17:06:58 +0200

    feat: extend example traffic

--- images/tedge/device_traffic.sh
+++ images/tedge/device_traffic.sh
@@ -3,7 +3,22 @@
 # Simulate some device traffic so the recorder has some mqtt traffic to record
 while :; do
     tedge mqtt pub 'tedge/events/ram' '{"text": "RAM is running a little low, but no cause for alarm just yet 😋"}' ||:
-    sleep 1
+    sleep 2
+    tedge mqtt pub 'tedge/events/healthy' '{"text": "Health check: OK"}' ||:
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 25}' ||:
+    sleep 0.5
     tedge mqtt pub 'tedge/measurements' '{"temperature": 26}' ||:
-    sleep 10
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 27}' ||:
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 28}' ||:
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 29}' ||:
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 30}' ||:
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 31}' ||:
+    sleep 0.5
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 32}' ||:
+    sleep 15
 done
