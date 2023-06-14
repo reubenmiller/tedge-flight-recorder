16882336f224b6e8de63d9b1e870f566b042a127
Reuben Miller <reuben.d.miller@gmail.com>
2023-06-14 15:17:50 +0200

    feat: add recorder script

--- images/tedge/device_traffic.sh
+++ images/tedge/device_traffic.sh
@@ -3,5 +3,7 @@
 # Simulate some device traffic so the recorder has some mqtt traffic to record
 while :; do
     tedge mqtt pub 'tedge/events/ram' '{"text": "RAM is running a little low, but no cause for alarm just yet 😋"}' ||:
-    sleep 10s
+    sleep 1
+    tedge mqtt pub 'tedge/measurements' '{"temperature": 26}' ||:
+    sleep 10
 done
